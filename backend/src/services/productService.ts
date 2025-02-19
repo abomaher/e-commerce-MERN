5fa13c7d515898fb24eefac19fa62a6043f071c9
Abdulhakim Zuqut <ahmz.1988@gmail.com>
2025-02-19 22:36:10 +0300

    All Products in Home Page + Integration with API

--- backend/src/services/productService.ts
+++ backend/src/services/productService.ts
@@ -1,26 +1,39 @@
 import { productModel } from "../models/productModel";
 
 export const getAllProducts = async () => {
-    return await productModel.find();
-}
+  return await productModel.find();
+};
 
 export const seedInitialProducts = async () => {
+  try {
+    const products = [
+      {
+        title: "Iphone 16 pro",
+        image:
+          "https://alephksa.com/cdn/shop/files/iPhone_16_Pro_Black_Titanium_PDP_Image_Position_1__en-ME_e3fe2d51-c0d4-46c7-98cc-d9bf76d98746.jpg",
+        price: 5200,
+        stock: 5,
+      },
+      {
+        title: "Imac 27 inch 2024",
+        image: "https://i.ebayimg.com/images/g/CyYAAOSw~2Rmv8qy/s-l1200.jpg",
+        price: 6000,
+        stock: 10,
+      },
+      {
+        title: "Toshiba Laptop",
+        image: "https://teletraders.net/wp-content/uploads/2021/06/sell-used-toshiba-laptops.png",
+        price: 1000,
+        stock: 3,
+      },
+    ];
 
-    try{
+    const existingProducts = await getAllProducts();
 
-        const products = [
-            { title: "Iphone 16 pro", image: "https://alephksa.com/cdn/shop/files/iPhone_16_Pro_Black_Titanium_PDP_Image_Position_1__en-ME_e3fe2d51-c0d4-46c7-98cc-d9bf76d98746.jpg", price: 5200, stock: 5 },
-            { title: "Imac 27 inch 2024", image: "https://i.ebayimg.com/images/g/CyYAAOSw~2Rmv8qy/s-l1200.jpg", price: 6000, stock: 10 }
-        ];
-    
-        const existingProducts = await getAllProducts();
-    
-        if(existingProducts.length === 0){
-            await productModel.insertMany(products);
-        }
-
-    } catch (err) {
-        console.error("Cannot seed database", err)
+    if (existingProducts.length === 0) {
+      await productModel.insertMany(products);
     }
-
-}
+  } catch (err) {
+    console.error("Cannot seed database", err);
+  }
+};
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,42 @@
-const HomePage = () =>{
-    return <div>Home page</div>
-}
+import { Box, Container, Grid } from "@mui/material";
+import ProductCard from "../components/ProductCard";
+import { useEffect, useState } from "react";
+import { Product } from "../types/Prodects";
+import { BASE_URL } from "../constants/BaseUrl";
+
+const HomePage = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try{
+            const response = await fetch(`${BASE_URL}/product`);
+            const data = await response.json();
+            setProducts(data);
+        } catch {
+            setError(true)
+        }
+    }
+
+    fetchData();
+  }, []);
+
+  if(error){
+    return <Box>Somthing went wrong, please try again!</Box>
+  }
+
+  return (
+    <Container sx={{ mt: 2 }}>
+      <Grid container spacing={2}>
+        {products.map((p) => (
+          <Grid item md={4}>
+            <ProductCard {...p} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
 
 export default HomePage;

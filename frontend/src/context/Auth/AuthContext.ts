88117a9ff481e85724b06a3b6e962c5eba45bb13
Abdulhakim Zuqut <ahmz.1988@gmail.com>
2025-02-20 03:03:14 +0300

    Logout

--- frontend/src/context/Auth/AuthContext.ts
+++ frontend/src/context/Auth/AuthContext.ts
@@ -3,15 +3,18 @@ import { createContext, useContext } from "react";
 interface AuthCntextType {
   username: string | null;
   token: string | null;
-  login: (username: string, token: string) => void;
   isAuthenticated: boolean;
+  login: (username: string, token: string) => void;
+  logout: () => void;
 }
 
 export const AuthContext = createContext<AuthCntextType>({
   username: null,
   token: null,
-  login: () => {},
   isAuthenticated: false,
+  login: () => {},
+  logout: () => {},
+
 });
 
 export const useAuth = () => useContext(AuthContext);
